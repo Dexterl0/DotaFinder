916b902f77e43749155af1b2461f59e7eb8e51ac
Dexter Liew <dexterliew0@gmail.com>
2020-11-19 20:46:59 +0000

    Added player data components and css

--- dota-finder/src/components/Building.js
+++ dota-finder/src/components/Building.js
@@ -4,15 +4,15 @@ import '../css/Building.css';
 const Building = () => {
     return (
         <div className="building">
-        <h4>Buildling Damage</h4>
-        <div className="building-score">
-            <p>2.5k</p>
-            <p>12.9</p>
-            <p>0</p>
-            <p>1.4k</p>
-            <p>296</p>
+            <h4>Buildling Damage</h4>
+            <div className="building-score">
+                <p>2.5k</p>
+                <p>12.9</p>
+                <p>0</p>
+                <p>1.4k</p>
+                <p>296</p>
+            </div>
         </div>
-    </div>
     );
 }
 
--- dota-finder/src/components/Team2.js
+++ dota-finder/src/components/Team2.js
@@ -1,10 +1,36 @@
 import React from 'react';
 import '../css/Team2.css';
+import Heroes from './Heroes';
+import Kills from './Kills';
+import Deaths from './Deaths';
+import Assists from './Assists';
+import Gold from './Gold';
+import LastHits from './LastHits';
+import Denies from './Denies';
+import Gpm from './Gpm';
+import Xpm from './Xpm';
+import Damage from './Damage';
+import Healing from './Healing';
+import Building from './Building';
 
 const Team2 = () => {
     return (
         <div>
             <h3 className="team-2-name">Dire</h3>
+            <div className="radiant-data">
+                <Heroes />
+                <Kills />
+                <Deaths />
+                <Assists />
+                <Gold />
+                <LastHits />
+                <Denies />
+                <Gpm />
+                <Xpm />
+                <Damage />
+                <Healing />
+                <Building />
+            </div>
         </div>
     );
 }
